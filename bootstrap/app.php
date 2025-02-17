5c58309f8b16fa9e86a5629e0c053509bb073a6c
Ruba <91896958+RubaUser@users.noreply.github.com>
2025-02-17 19:11:13 +0300

    add admin and user Login and register views

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'user' => \App\Http\Middleware\UserMiddleware::class,
+            // 'clear_cookies' => \App\Http\Middleware\ClearCookies::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
